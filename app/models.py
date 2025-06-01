09dd34683eeab6f03731c30c4f42da1f1ab872ec
Vadimka180880 <vadim.te180880@gmail.com>
2025-06-01 12:07:24 -0400

    feat: initial migration and contacts table created

---
+++ app/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, Date
+from app.database import Base 
+
+class Contact(Base):
+    __tablename__ = 'contacts'
+
+    id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String(55), nullable=False)
+    last_name = Column(String(55), nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
+    phone_number = Column(String(20), nullable=False)
+    birthday = Column(Date, nullable=False)
+    additional_info = Column(String(250), nullable=True)
